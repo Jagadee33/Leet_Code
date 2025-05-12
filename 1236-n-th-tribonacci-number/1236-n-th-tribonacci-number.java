c66507baed25893e7405d36eb994ab643206c9f2
Jagadee33 <122670227+Jagadee33@users.noreply.github.com>
2025-05-12 10:03:17 +0530

    Time: 0 ms (100%), Space: 40.8 MB (9.97%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    int[] k = new int[38];
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1||n==2) return 1;
+        if(k[n] != 0) return k[n];
+        k[n] = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        return k[n];
+
+    }
+}

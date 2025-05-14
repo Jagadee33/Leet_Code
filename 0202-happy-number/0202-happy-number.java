332dd2fa19bc8e449d332e431e431ab8718e9e98
Jagadee33 <122670227+Jagadee33@users.noreply.github.com>
2025-05-14 09:50:43 +0530

    Time: 2 ms (19.55%), Space: 40.9 MB (52.02%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getnewnum(int n){
+        int output = 0;
+        while(n>0){
+            int digit = n % 10;
+            output += digit * digit;
+            n = n / 10;
+        }
+        return output;
+    }
+
+    public boolean isHappy(int n) {
+        Set <Integer> map = new HashSet<>();
+        while(!map.contains(n)){
+            map.add(n);
+            n = getnewnum(n);
+            if(n ==1) return true;
+        }
+        return false;
+    }
+}

0758d529ecf0bc5d05fff356d5fe6e1eb9b041bc
Jagadee33 <122670227+Jagadee33@users.noreply.github.com>
2025-05-21 09:40:40 +0530

    Time: 405 ms (5.01%), Space: 40.7 MB (41.16%) - LeetHub

--- 2037-count-square-sum-triples/2037-count-square-sum-triples.java
+++ 2037-count-square-sum-triples/2037-count-square-sum-triples.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countTriples(int n) {
         int count =0;
-        for(int i =1;i<=n;i++){
-            for(int j =1;j<=n;j++){
-                int sumofsqr = i*i+j*j;
-                int c = (int)Math.sqrt(sumofsqr);
-                if(c<=n && c*c==sumofsqr){
-                    count++;
+        for(int a = 1; a<=n;a++){
+            for(int b =1;b<=n;b++){
+                for(int c=1;c<=n;c++){
+                    if(c<=n && c*c==a*a+b*b){
+                        count++;
+                    }
                 }
             }
         }return count;
